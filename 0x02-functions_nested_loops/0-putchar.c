62b230a2eb61e858e2e7c7d60206f9dd346e447f
Archibald <archibaldkhojane@gmail.com>
2023-02-21 06:04:32 -0800

    Functions n Loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,13 +8,14 @@
 
 int main(void)
 {
-	char _putchar[8] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
-	unsigned int c;
-
-	for (c = 0; c < sizeof(_putchar); c++)
-	{
-		_putchar(_putchar[c]);
-	}
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
 _putchar('\n');
 return (0);
 }
