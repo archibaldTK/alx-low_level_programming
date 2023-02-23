e4946ebeea218aad830ec406791b1c7d87f47b9c
Archibald <archibaldkhojane@gmail.com>
2023-02-23 08:07:26 -0800

    Functions n Nest Loops

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -11,13 +11,13 @@ void print_most_numbers(void)
 {
 	char c = '0';
 
-	while (c <= '9');
+	while (c <= '9')
 	{
-		if (c != '2' && c != '4');
-			{
+		if (c != '2' && c != '4')
+		{
 			_putchar(c);
-			}
-		c++;
 		}
-	_putchar('\n');
+	c++;
+	}
+_putchar('\n');
 }
