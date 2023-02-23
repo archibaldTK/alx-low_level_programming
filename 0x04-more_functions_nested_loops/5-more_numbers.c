a59696aa5660f5b6ee93cf121117ee9d4a000ee2
Archibald <archibaldkhojane@gmail.com>
2023-02-23 06:48:30 -0800

    Functions n Nest Loops

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * more_numbers - prints 10 times the numbers, 0 to 14
+ * code by ArchibaldTK
+ * Return: 0
+ */
+
+void more_numbers(void)
+{
+	int a = 0;
+	int b = 0;
+
+	while (b <= 9)
+	{
+		while (a <= 14)
+		{
+			if (a > 9)
+				_putchar(a / 10 + '0');
+			_putchar(a % 10 + '0');
+			a++;
+		}
+		_putchar('\n');
+		b++;
+		a = 0;
+	}
+}
