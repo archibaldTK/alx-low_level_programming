22dd7ccc024c61d6bcec619786eb938c40be6472
Archibald <archibaldkhojane@gmail.com>
2023-02-23 06:25:08 -0800

    Functions n Nest Loops

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers, from 0 - 9
+ * code by ArchibaldTK
+ * Return: 0
+*/
+
+void print_numbers(void)
+{
+	char c = '0';
+
+	while (c <= '9')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n')
+}
