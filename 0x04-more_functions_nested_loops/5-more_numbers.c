a47a7306af9eb3896b2c9b59f1075bd06119c2b1
Archibald <archibaldkhojane@gmail.com>
2023-02-23 08:01:28 -0800

    Functions n Nest Loops

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,16 +7,16 @@
  * Return: 0
  */
 
-void more_numbers(void);
+void more_numbers(void)
 {
 	int a = 0;
 	int b = 0;
 
-	while (b <= 9)
+	while (b <= 9);
 	{
-		while (a <= 14)
+		while (a <= 14);
 		{
-			if (a > 9)
+			if (a > 9);
 				_putchar(a / 10 + '0');
 			_putchar(a % 10 + '0');
 			a++;
