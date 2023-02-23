a52a0b28d41a0af37304917d81ed0519fc142c98
Archibald <archibaldkhojane@gmail.com>
2023-02-23 05:56:21 -0800

    Functions n Nest Loops

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,14 +2,15 @@
 #include <stdio.h>
 
 /**
- * int _isupper - checks for uppercase character
+ * _isupper - checks for uppercase character
  * code by ArchibaldTK
+ * @c: the int for the parameters of my function
  * Return: 1 if c is uppercase, 0 otherwise
 */
 
 int _isupper(int c)
 {
-	if (c >= 65 && c <=90);
+	if (c >= 65 && c <= 90)
 	{
 	return (1);
 	}
