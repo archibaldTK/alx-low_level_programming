c9cba8db8e6d4e5e6747894c18960e9c8d415ae4
Archibald <archibaldkhojane@gmail.com>
2023-02-23 06:15:32 -0800

    Functions n Nest Loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * code by ArchibaldTK
+ * @a: the int for the parameters of my function
+ * @b: the int for the parameters of my function
+ * Return: 0
+*/
+
+int mul(int a, int b);
+{
+	return (a * b);
+}
