23eae1e51b5c626c6b56a0649791ac775cc02a0d
Archibald <archibaldkhojane@gmail.com>
2023-02-23 14:22:42 -0800

    Functions n Nest Loops

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -14,12 +14,12 @@ void print_line(int n)
 	
 	if (n > 0)
 	{
-	for (i = 0; i < n; i++)
-	{
-	_putchar('_');
-	}
-	_putchar('\n');
-	}	
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+			}
+		_putchar('\n');
+		}	
 	else
 	_putchar('\n');
 }
