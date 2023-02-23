a7b7601a0c106547416cb52f97668fb7b4a50ae1
Archibald <archibaldkhojane@gmail.com>
2023-02-23 05:47:14 -0800

    Functions n Nest Loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * int _isupper - checks for uppercase character
+ * code by ArchibaldTK
+ * Return: 1 if c is uppercase, 0 otherwise
+*/
+
+int _supper(int c)
+{
+	if (c >= 65 && c <=90);
+	{
+	return (1);
+	}
+	else
+	return (0);
+}
