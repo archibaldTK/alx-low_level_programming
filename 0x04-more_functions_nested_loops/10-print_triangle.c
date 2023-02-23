edb2a0eca9469127621655f01b981ad645950809
Archibald <archibaldkhojane@gmail.com>
2023-02-23 08:51:19 -0800

    Functions n Nest Loops

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_triangle - prints a triangle
+ * @size: the int for the parameters of the function
+ * code by ArchibaldTK
+ * Return: 0
+ */
+
+void print_triangle(int size)
+{
+	int x, y = 0;
+	int space;
+
+	if (size > 0)
+	{
+		while (y < size)
+		{
+			space = size - y - 1;
+			while (x < size)
+			{
+				if (space > x)
+				{
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar('#');
+				}
+				x++;
+			}
+			y++;
+			x = 0;
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
