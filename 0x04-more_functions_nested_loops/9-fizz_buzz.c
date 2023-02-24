9a71ad27a0cc0afe3a568fd044177d26555cb740
Archibald <archibaldkhojane@gmail.com>
2023-02-24 00:00:16 -0800

    Functions n Nest Loops

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -18,7 +18,7 @@ int main(void)
 	while (h <= 100)
 	{
 		if (h % 3 == 0 && h % 5 == 0)
-			printf("FIzzBuzz ");
+			printf(" FizzBuzz");
 		else if (h % 5 == 0)
 		{
 			if (h == 100)
@@ -27,10 +27,10 @@ int main(void)
 				printf("\n");
 			}
 			else
-				printf("Buzz ");
+				printf(" Buzz");
 		}
 		else if (h % 3 == 0)
-			printf("Fizz ");
+			printf(" Fizz");
 		else
 			printf("%d ", h);
 	h++;
