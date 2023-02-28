1ba4638d8afde642c5c1bb5a5a44bf0e0d0097ad
Archibald <archibaldkhojane@gmail.com>
2023-02-28 08:04:13 -0800

    Pointers Arrays n Strings

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * reset_to_98 - a pointer to an int as parameter and updates the value it points to to 98
+ * reset_to_98 - pointer to an int as parameter & updates the value it points to to 98
  * @n: the int paramters for the function reset_to_98
  * code by ArchibaldTK
  * Return: Always 0.
