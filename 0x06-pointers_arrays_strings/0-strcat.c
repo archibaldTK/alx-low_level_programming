47a4db9995401199047db8f257c6e17bad68272a
Archibald <archibaldkhojane@gmail.com>
2023-03-01 01:41:31 -0800

    Pointers Arrays n Strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strcat - function that concatenates two strings
+ * appends the src string to the dest string,
+ * overwriting the terminating null byte (\0) at the end of dest
+ * code by ArchibaldTK
+ * @dest: the input value
+ * @scr: the input value
+ * Return: 0
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
