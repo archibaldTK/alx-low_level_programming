e57a82e203d03fe97fe2de6ba040031bd8650b18
Archibald <archibaldkhojane@gmail.com>
2023-03-06 05:39:15 -0800

    More Functions Arrays n Strings

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -3,24 +3,32 @@
 /**
  * _strpbrk - function that searches a string for any of a set of bytes.
  * code by ArchibaldTK
- * @s: input
- * @accept: input
- * Return: Always 0 (Success)
+ * @s: string
+ * @accept: string to match
+ * Return: Pointer to the byte in `s` that matches one of the bytes in `accept`
+ * or NULL if no such byte is found.
  */
 
 char *_strpbrk(char *s, char *accept)
 {
-		int k;
+	int i, j;
+	char *p;
 
-		while (*s)
+	i = 0;
+	while (s[i] != '\0')
+	{
+		j = 0;
+		while (accept[j] != '\0')
 		{
-			for (k = 0; accept[k]; k++)
+			if (accept[j] == s[i])
 			{
-			if (*s == accept[k])
-			return (s);
+				p = &s[i];
+				return (p);
 			}
-		s++;
+			j++;
 		}
+		i++;
+	}
 
-	return ('\0')
+	return (0);
 }
