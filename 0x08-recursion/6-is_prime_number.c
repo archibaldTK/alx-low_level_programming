558ff7fedea50f65a1d213cf79c1abd17bf64be1
Archibald <archibaldkhojane@gmail.com>
2023-03-08 07:10:47 -0800

    Recursion

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -7,39 +7,25 @@
  * Return: 0
  */
 
-int sqtRecursive(int n, int m)
-{
-	if (n <= 0)
-		return (-1);
-	if (n * n == m)
-		return (n);
-	return (sqtRecursive(n - 1, m));
-}
-
-/**
- * _sqrt_recursion - finds the natural square root of a number
- * @n: given number
- * Return: square root of n or -1
- **/
-
-int _sqrt_recursion(int n)
-{
-	if (n == 1)
-		return (1);
-	return (sqtRecursive(n / 2, n));
-}
-
-/**
- * is_prime_number - checks if a given number is prime
- * @n: given number
- * Return: 1 if number is prime else 0
- **/
-
 int is_prime_number(int n)
 {
-	if (n <= 1 || _sqrt_recursion(n) >= 1)
+	return (helper_prime(n, 2, n / 2));
+}
+
+/**
+ *  helper_prime - blank
+ * @n: checkl
+ * @i: for the increment
+ * @limit: stop
+ * Return: 0
+ */
+
+int helper_prime(int n, int i, int limit)
+{
+	if ((n % i == 0 && i <= limit) || n < 0 || n == 1)
 		return (0);
-	if (_sqrt_recursion(n) == -1)
+	else if (n % i != 0 && i <= limit)
+		return (helper_prime(n, i + 1, limit));
+	else
 		return (1);
-	return (_sqrt_recursion(n));
 }
