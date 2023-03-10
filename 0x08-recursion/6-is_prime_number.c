9b471cf7822417b17c50431d25a23c3a8d137a74
Archibald <archibaldkhojane@gmail.com>
2023-03-09 22:15:10 -0800

    Recursion

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,42 +1,35 @@
 #include "main.h"
 
-/*
- * sqtRecursive - computes square root recursively
+/**
+ * is_prime - detects if an input number is a prime number.
  * code by ArchibaldTK
- * @n: given number
- * @m: comparison number
- * Return: 1 if not found sqrroot, else sqrroot
+ * @n: input number.
+ * @c: iterator.
+ * Return: 1 if n is a prime number. 0 if n is not a prime number.
  */
-
-int sqtRecursive(int n, int m)
+int is_prime(unsigned int n, unsigned int c)
 {
-	if (n <= 0)
-		return (-1);
-	if (n * n == m)
-		return (n);
-	return (sqtRecursive(n - 1, m));
+	if (n % c == 0)
+	{
+		if (n == c)
+			return (1);
+		else
+			return (0);
+	}
+	return (0 + is_prime(n, c + 1));
 }
 /**
- * _sqrt_recursion - finds the natural square root of a number
- * @n: given number
- * Return: square root of n or -1
- **/
-int _sqrt_recursion(int n)
-{
-	if (n == 1)
-		return (1);
-	return (sqtRecursive(n / 2, n));
-}
-/**
- * is_prime_number - checks if a given number is prime
- * @n: given number
- * Return: 1 if number is prime else 0
- **/
+ * is_prime_number - detects if an input number is a prime number.
+ * @n: input number.
+ * Return: 1 if n is a prime number. 0 if n is not a prime number.
+ */
 int is_prime_number(int n)
 {
-	if (n <= 1 || _sqrt_recursion(n) >= 1)
+	if (n == 0)
 		return (0);
-	if (_sqrt_recursion(n) == -1)
-		return (1);
-	return (_sqrt_recursion(n));
+	if (n < 0)
+		return (0);
+	if (n == 1)
+		return (0);
+	return (is_prime(n, 2));
 }
