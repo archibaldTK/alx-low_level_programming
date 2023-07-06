8fa67701ffa850a0b78cf5c8dbdbca4d82911803
ArchibaldTK <archibaldkhojane@gmail.com>
2023-07-06 09:45:15 -0700

    Bit Manipulation

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,26 +1,20 @@
 #include "main.h"
-
 /**
- * flip_bits - counts the number of bits to change
- * to get from one number to another
+ * flip_bits - flips bits
  * code by ArchibaldTK
  * @n: first number
  * @m: second number
- *
- * Return: number of bits to change
+ * Return: unsigned int value of number
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	int khojane, khojane = 0;
-	unsigned long int current;
-	unsigned long int exclusive = n ^ m;
+	unsigned long int flips, count = 0;
 
-	for (archibald = 63; archibald >= 0; archibald--)
+	flips = n ^ m;
+	while (flips > 0)
 	{
-		current = exclusive >> archibald;
-		if (current & 1)
-			khojane++;
+		count += flips & 1;
+		flips >>= 1;
 	}
-
-	return (khojane);
+	return (count);
 }
